a44ed6fc8c92b98b23fb326e91354ec87736528c
Dhruvil Patel <thedhruvilpatel@gmail.com>
2017-10-02 20:54:23 -0500

    Homework

---
+++ 01week/datatypes.js
@@ -0,0 +1,15 @@
+
+let displayDate = function (){
+  var date = new Date();
+  console.log(date)
+}
+
+displayDate();
+
+let numberToString = function (){
+  if (typeof(x)==='number');{
+    console.log(x.toString() + "its a string");
+  }
+}
+
+();
